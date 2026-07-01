1792086d177376238d7952a77409770f1f1a089d
Vonware <systemdeveloper@vonware.net>
2026-07-01 11:32:18 -0500

    Crud Clientes

--- src/modules/clientes/logic/clientes.logic.ts
+++ src/modules/clientes/logic/clientes.logic.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { mapListResult } from '../../../common/helpers/auth-response.helper';
+import { mapDeleteResult, mapListResult, mapSingleResult } from '../../../common/helpers/auth-response.helper';
 import { FiltroClienteDto } from '../dto/filtros-cliente.dto';
 import { ClientesModel } from '../models/clientes.model';
+import { ValidarDocumentoClienteDto } from '../dto/validar-documento.dto';
+import { ResponseHelper } from 'src/common/helpers/response.helper';
+import { CreateClienteDto, UpdateClienteDto } from '../dto/crear-cliente.dto';
 
 @Injectable()
 export class ClientesLogic {
@@ -10,5 +13,35 @@ export class ClientesLogic {
   async listar(filtros: FiltroClienteDto) {
     const result = await this.clientesModel.listar(filtros);
     return mapListResult(result, filtros);
+  }
+
+  async obtenerPorId(id: number) {
+    const result = await this.clientesModel.obtenerPorId(id);
+    return mapSingleResult(result, `Cliente ${id} no encontrado`);
+  }
+
+  async eliminar(id: number) {
+    const result = await this.clientesModel.eliminar(id);
+    return mapDeleteResult(result, `Cliente ${id} no encontrado o ya está inactivo`);
+  }
+
+  async restaurar(id: number) {
+    const result = await this.clientesModel.restaurar(id);
+    return mapDeleteResult(result, `Cliente ${id} no encontrado o ya está activo`);
+  }
+
+  async validarDocumento(dto: ValidarDocumentoClienteDto) {
+    const result = await this.clientesModel.validarDocumento(dto.numeroDocumento,dto.idExcluir,);
+    return ResponseHelper.success(result, 'Validación de documento realizada');
+  }
+
+  async crear(dto: CreateClienteDto) {
+    const result = await this.clientesModel.crear(dto);
+    return mapSingleResult(result, 'No se pudo crear el cliente');
+  }
+
+  async actualizar(id: number, dto: UpdateClienteDto) {
+    const result = await this.clientesModel.actualizar(id, dto);
+    return mapSingleResult(result, `Cliente ${id} no encontrado`);
   }
 }
