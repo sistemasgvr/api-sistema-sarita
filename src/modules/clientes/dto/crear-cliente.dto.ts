9bef5d3621ea3774d350ff14e046615b379517e1
Emmanuel-RD <emmanuelrd2003@gmail.com>
2026-07-01 12:05:20 -0500

    Reapply "Crud Clientes"
    This reverts commit 9d51e33ffd6318c12f54e18cecc4d01352f61bd8.

---
+++ src/modules/clientes/dto/crear-cliente.dto.ts
@@ -0,0 +1,155 @@
+import { ApiProperty, ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEmail,
+  IsInt,
+  IsOptional,
+  IsString,
+  MaxLength,
+  ValidateIf,
+} from 'class-validator';
+import { AuditoriaDto } from '../../../common/dto/auditoria.dto';
+
+export class CreateClienteDto extends AuditoriaDto {
+  @ApiPropertyOptional({ maxLength: 50 })
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  codigoInterno?: string;
+
+  @ApiPropertyOptional({ example: 'Comercial Los Andes S.A.C.' })
+  @ValidateIf((o) => !o.nombres)
+  @IsString()
+  @MaxLength(200)
+  razonSocial?: string;
+
+  @ApiPropertyOptional({ example: 'Juan' })
+  @ValidateIf((o) => !o.razonSocial)
+  @IsString()
+  @MaxLength(150)
+  nombres?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  apellidoPaterno?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(150)
+  apellidoMaterno?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idTipoCliente?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idTipoPersona?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idTipoDocumento?: number;
+
+  @ApiPropertyOptional({ example: '12345678' })
+  @IsOptional()
+  @IsString()
+  @MaxLength(20)
+  numeroDocumento?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(250)
+  direccion?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(250)
+  referencia?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(20)
+  telefono?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsEmail()
+  @MaxLength(150)
+  email?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idDepartamento?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idProvincia?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idDistrito?: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  idPais?: number;
+
+  @ApiPropertyOptional({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  esAgentePercepcion?: boolean;
+
+  @ApiPropertyOptional({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  esBuenContribuyente?: boolean;
+
+  @ApiPropertyOptional({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  esAgenteRetenedor?: boolean;
+
+  @ApiPropertyOptional({ default: false })
+  @IsOptional()
+  @IsBoolean()
+  afectoRus?: boolean;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  situacionSunat?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(50)
+  estadoContribuyenteSunat?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  observacion?: string;
+}
+
+export class UpdateClienteDto extends PartialType(CreateClienteDto) {}
--- src/modules/clientes/models/clientes.model.ts
+++ src/modules/clientes/models/clientes.model.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '../../../database/database.service';
 import { FiltroClienteDto } from '../dto/filtros-cliente.dto';
-import { AuthListResult } from '../../../common/interfaces/auth-db.interface';
+import { AuthDeleteResult, AuthExisteResult, AuthListResult, AuthSingleResult } from '../../../common/interfaces/auth-db.interface';
+import { CreateClienteDto, UpdateClienteDto } from '../dto/crear-cliente.dto';
 
 @Injectable()
 export class ClientesModel {
@@ -13,6 +14,91 @@ export class ClientesModel {
       filtros.busqueda ?? null,
       filtros.limite ?? 50,
       filtros.pagina ?? 1,
+    ]);
+  }
+  obtenerPorId(id: number) {
+    return this.db.callFunctionJson<AuthSingleResult<any>>('cli_obtener_por_id_cliente', [id]);
+  }
+
+  eliminar(id: number, idUsuarioAuditoria?: number) {
+    return this.db.callFunctionJson<AuthDeleteResult>('cli_eliminar_logico_cliente', [
+      id,
+      idUsuarioAuditoria ?? null,
+    ]);
+  }
+
+  restaurar(id: number, idUsuarioAuditoria?: number) {
+    return this.db.callFunctionJson<AuthDeleteResult>('cli_restaurar_cliente', [
+      id,
+      idUsuarioAuditoria ?? null,
+    ]);
+  }
+
+  validarDocumento(numeroDocumento: string, idExcluir?: number) {
+    return this.db.callFunctionJson<AuthExisteResult>('cli_validar_documento_cliente', [
+      numeroDocumento,
+      idExcluir ?? null,
+    ]);
+  }
+
+  crear(dto: CreateClienteDto) {
+    return this.db.callFunctionJson<AuthSingleResult<any>>('cli_crear_clientes', [
+      dto.codigoInterno ?? null,
+      dto.razonSocial ?? null,
+      dto.idTipoCliente ?? null,
+      dto.idTipoPersona ?? null,
+      dto.nombres ?? null,
+      dto.apellidoPaterno ?? null,
+      dto.apellidoMaterno ?? null,
+      dto.idTipoDocumento ?? null,
+      dto.numeroDocumento ?? null,
+      dto.direccion ?? null,
+      dto.referencia ?? null,
+      dto.telefono ?? null,
+      dto.email ?? null,
+      dto.idDepartamento ?? null,
+      dto.idProvincia ?? null,
+      dto.idDistrito ?? null,
+      dto.idPais ?? null,
+      dto.esAgentePercepcion ?? false,
+      dto.esBuenContribuyente ?? false,
+      dto.esAgenteRetenedor ?? false,
+      dto.afectoRus ?? false,
+      dto.situacionSunat ?? null,
+      dto.estadoContribuyenteSunat ?? null,
+      dto.observacion ?? null,
+      dto.idUsuarioAuditoria ?? null,
+    ]);
+  }
+
+  actualizar(id: number, dto: UpdateClienteDto) {
+    return this.db.callFunctionJson<AuthSingleResult<any>>('cli_actualizar_por_id_cliente', [
+      id,
+      dto.codigoInterno ?? null,
+      dto.razonSocial ?? null,
+      dto.idTipoCliente ?? null,
+      dto.idTipoPersona ?? null,
+      dto.nombres ?? null,
+      dto.apellidoPaterno ?? null,
+      dto.apellidoMaterno ?? null,
+      dto.idTipoDocumento ?? null,
+      dto.numeroDocumento ?? null,
+      dto.direccion ?? null,
+      dto.referencia ?? null,
+      dto.telefono ?? null,
+      dto.email ?? null,
+      dto.idDepartamento ?? null,
+      dto.idProvincia ?? null,
+      dto.idDistrito ?? null,
+      dto.idPais ?? null,
+      dto.esAgentePercepcion ?? null,
+      dto.esBuenContribuyente ?? null,
+      dto.esAgenteRetenedor ?? null,
+      dto.afectoRus ?? null,
+      dto.situacionSunat ?? null,
+      dto.estadoContribuyenteSunat ?? null,
+      dto.observacion ?? null,
+      dto.idUsuarioAuditoria ?? null,
     ]);
   }
 }
